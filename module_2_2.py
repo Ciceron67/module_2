c2aa4a926cfc17864045357e149fe975d17ef374
Ciceron67 <ciceron111185@gmail.com>
2024-07-23 01:12:06 +0300

    Delete module_2_2.py

--- module_2_2.py
+++
@@ -1,9 +0,0 @@
-first = input('Введите первое число: ')
-second = input('Введите второе число: ')
-thrid = input('Введите третье число: ')
-if first == second == thrid:
-    print(3)
-elif first == second or second == thrid or first == thrid:
-    print(2)
-else:
-    print(0)

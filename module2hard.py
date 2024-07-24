0384efc3b558ed55ec27253415d493536d3ccc1d
Ciceron67 <ciceron111185@gmail.com>
2024-07-24 19:54:13 +0300

    Update module2hard.py

--- module2hard.py
+++ module2hard.py
@@ -4,7 +4,7 @@ num = random.randint(3, 20)
 result = []
 
 for i in range(num):
-    # lst.append(i)
+    
     for j in range(i + 1, num):
         if num % (j + i) == 0 and i != 0 and j != 0:
             result.append(i)

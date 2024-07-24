4031fe166c3c25273523067b1de98692b50d1ffb
Ciceron67 <ciceron111185@gmail.com>
2024-07-24 19:52:33 +0300

    111

---
+++ module2hard.py
@@ -0,0 +1,13 @@
+import random
+
+num = random.randint(3, 20)
+result = []
+
+for i in range(num):
+    # lst.append(i)
+    for j in range(i + 1, num):
+        if num % (j + i) == 0 and i != 0 and j != 0:
+            result.append(i)
+            result.append(j)
+
+print(num, '-', result)
